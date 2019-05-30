0261a21fddc847e60d5940bc0bee4dd12bc4b411
bhonnef <honnef@bhonnef-pc>
2019-05-30 00:35:41 -0300

    adicionado novos arquivos de base

--- include/system/traits.h
+++ include/system/traits.h
@@ -46,9 +46,9 @@ struct Traits
 template<> struct Traits<Build>: public Traits<void>
 {
     static const unsigned int MODE = LIBRARY;
-    static const unsigned int ARCHITECTURE = IA32;
-    static const unsigned int MACHINE = PC;
-    static const unsigned int MODEL = Legacy_PC;
+    static const unsigned int ARCHITECTURE = ARMv7;
+    static const unsigned int MACHINE = Cortex_M;
+    static const unsigned int MODEL = LM3S811;
     static const unsigned int CPUS = 1;
     static const unsigned int NODES = 1; // > 1 => NETWORKING
 };
