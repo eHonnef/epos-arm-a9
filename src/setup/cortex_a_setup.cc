0b904270c244f2a28ae54683bbaa8efa495b350b
bhonnef <honnef@bhonnef-pc>
2019-06-02 01:49:10 -0300

    peguei o assembly de cortex_setup.s no svn

--- src/setup/cortex_a_setup.cc
+++ src/setup/cortex_a_setup.cc
@@ -9,43 +9,39 @@ void _vector_table() __attribute__((used, naked, section(".init")));
 // Interrupt Vector Table
 void _vector_table() {
     ASM("\t\n\
-        .equ MODE_FIQ, 0x11 \t\n\
-        .equ MODE_IRQ, 0x12 \t\n\
-        .equ MODE_SVC, 0x13 \t\n\
-\t\n\
-        //.section .vector_table, 'x' \t\n\
-        .global _Reset \t\n\
-        .global _start \t\n\
-\t\n\
-        _Reset: \t\n\
-        b _reset                                                           \t\n\
-        b abort  //_undefined_instruction                                           \t\n\
-        b .      //_software_interrupt                  \t\n\
-        b abort  //_prefetch_abort                                                  \t\n\
-        b abort  //_data_abort                                                      \t\n\
-        nop      // Reserved                            \t\n\
-        b   _irq                                                             \t\n\
-        b   .    //_fiq                                                            \t\n\
-\t\n\
-        _reset: \t\n\
-        // Change the vector table base address \t\n\
-        ldr r0, =0x60000000 \t\n\
-        mcr p15, #0, r0, c12, c0, #0 \t\n\
-\t\n\
-        // FIQ stack\t\n\
-        //msr cpsr_c, MODE_FIQ \t\n\
-        //ldr r1, =_fiq_stack_start \t\n\
-        //ldr sp, =_fiq_stack_end \t\n\
-        //movw r0, #0xFEFE \t\n\
-        //movt r0, #0xFEFE \t\n\
-\t\n\
-        abort: \t\n\
-        swi 0xFF \t\n\
-\t\n\
-        .global _irq \t\n\
-        _irq: \t\n\
-        ldr r0, =0x10009000 \t\n\
-        ldr r1, [r0] \t\n\
-        b . \t\n\
+        // EPOS Cortex-A SETUP                          \t\n\
+                                                        \t\n\
+        //.file cortex_a_setup.S                        \t\n\
+                                                        \t\n\
+        // Interrupt Vector Table                       \t\n\
+        .section .init                                  \t\n\
+        .type _vector_table, object                     \t\n\
+                                                        \t\n\
+        _vector_table:                                  \t\n\
+        ldr pc, _start_addr                             \t\n\
+        ldr pc, _undefined_instruction_addr             \t\n\
+        ldr pc, _swi_addr                               \t\n\
+        ldr pc, _prefetch_abort_addr                    \t\n\
+        ldr pc, _data_abort_addr                        \t\n\
+        ldr pc, _reserved_addr                          \t\n\
+        ldr pc, _irq_addr                               \t\n\
+        ldr pc, _fiq_addr                               \t\n\
+                                                        \t\n\
+        _start_addr:                                    \t\n\
+                .word _start                            \t\n\
+        _undefined_instruction_addr:                    \t\n\
+                .word _undefined_instruction            \t\n\
+        _swi_addr:                                      \t\n\
+                .word _software_interrupt               \t\n\
+        _prefetch_abort_addr:                           \t\n\
+                .word _prefetch_abort                   \t\n\
+        _data_abort_addr:                               \t\n\
+                .word _data_abort                       \t\n\
+        _reserved_addr:                                 \t\n\
+                .word _reserved                         \t\n\
+        _irq_addr:                                      \t\n\
+                .word _int_entry                        \t\n\
+        _fiq_addr:                                      \t\n\
+                .word _fiq                              \t\n\
     ");
 }
